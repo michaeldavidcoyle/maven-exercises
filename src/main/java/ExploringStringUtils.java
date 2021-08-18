1d3103b9dd1c4e20162f6ffe465f20d4145e4f24
Michael Coyle <michael.david.coyle4@gmail.com>
2021-08-18 10:54:36 -0500

    Updated to imitate example output in curriculum

--- src/main/java/ExploringStringUtils.java
+++ src/main/java/ExploringStringUtils.java
@@ -23,6 +23,15 @@ public class ExploringStringUtils {
         System.out.print("Enter something: ");
         String userInput = input.nextLine();
 
-        System.out.println(StringUtils.reverse(userInput));
+        System.out.printf("You entered: \"%s\"%n", userInput);
+
+        if (StringUtils.isNumeric(userInput)) {
+            System.out.printf("\"%s\" is a number.%n", userInput);
+        } else {
+            System.out.printf("\"%s\" is not a number.%n", userInput);
+        }
+
+        System.out.printf("Flipped case: %s%n", StringUtils.swapCase(userInput));
+        System.out.printf("Reversed: %s%n", StringUtils.reverse(userInput));
     }
 }
